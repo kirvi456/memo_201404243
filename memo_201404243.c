11bfaf62790803f8098199b4de9c365a651b9ba6
kirvi <41593385+kirvi456@users.noreply.github.com>
2019-09-15 19:57:42 -0600

    Update memo_201404243.c

--- memo_201404243.c
+++ memo_201404243.c
@@ -7,9 +7,7 @@
 #include <linux/kernel.h>   
 #include <linux/fs.h>
 
-
-#define NOMBREPROCESO "memo_201404243"
-#define BUFSIZE  150
+#define BUFSIZE  	150
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Escribir informaciond de la memoria ram.");
@@ -17,22 +15,22 @@ MODULE_AUTHOR("Bryan Ordonez -  201404243");
 
 struct sysinfo inf;
 
-static int escribir_archivo(struct seq_file *m, void *v) {	
-	si_meminfo(&inf);
-    long total_memoria = (inf.totalram * 4);
-    long memoria_libre = (inf.freeram * 4 );
-    seq_printf(m, " __________________________________________________\n");
-    seq_printf(m, "| Lab. Sistemas Operativos 1                       |\n");
-    seq_printf(m, "| Segundo Semestre 2019                            |\n");
-    seq_printf(m, "| Bryan Otoniel Ordoñez Morales                    |\n");
-    seq_printf(m, "| 2014-04243                                       |\n");
-    seq_printf(m, "|                                                  |\n");
-    seq_printf(m, "|    PRACTICA 2 (MODULO 1 - MEMORIA RAM)           |\n");
-    seq_printf(m, "|__________________________________________________|\n");
-    seq_printf(m, "  Sistema Operativo: CentOS 7\n");
-    seq_printf(m, "  Memoria Total : \t %8lu KB - %8lu MB\n",total_memoria, total_memoria / 1024);
-    seq_printf(m, "  Memoria Libre : \t %8lu KB - %8lu MB \n", memoria_libre, memoria_libre / 1024);
-    seq_printf(m, "  Memoria en uso: \t %i %%\n", (memoria_libre * 100)/total_memoria) ;
+static int escribir_archivo(struct seq_file * archivo, void *v) {	
+    si_meminfo(&inf);
+    long total_memoria 	= (inf.totalram * 4);
+    long memoria_libre 	= (inf.freeram * 4 );
+    seq_printf(archivo, " __________________________________________________\n");
+    seq_printf(archivo, "| Lab. Sistemas Operativos 1                       |\n");
+    seq_printf(archivo, "| Segundo Semestre 2019                            |\n");
+    seq_printf(archivo, "| Bryan Otoniel Ordoñez Morales                    |\n");
+    seq_printf(archivo, "| 2014-04243                                       |\n");
+    seq_printf(archivo, "|                                                  |\n");
+    seq_printf(archivo, "|    PRACTICA 2 (MODULO 1 - MEMORIA RAM)           |\n");
+    seq_printf(archivo, "|__________________________________________________|\n");
+    seq_printf(archivo, "  Sistema Operativo: CentOS 7\n");
+    seq_printf(archivo, "  Memoria Total : \t %8lu KB - %8lu MB\n",total_memoria, total_memoria / 1024);
+    seq_printf(archivo, "  Memoria Libre : \t %8lu KB - %8lu MB \n", memoria_libre, memoria_libre / 1024);
+    seq_printf(archivo, "  Memoria en uso: \t %i %%\n", (memoria_libre * 100)/total_memoria) ;
     return 0;
 }
 
@@ -48,14 +46,14 @@ static struct file_operations operaciones =
 
 static int iniciar(void)
 {
-    proc_create(NOMBREPROCESO,0,NULL,&operaciones);
+    proc_create("memo_201404243", 0, NULL, &operaciones);
     printk(KERN_INFO "Carnet: 2014-04243\n");
     return 0;
 }
  
 static void salir(void)
 {
-    remove_proc_entry(NOMBREPROCESO,NULL);
+    remove_proc_entry("memo_201404243", NULL);
     printk(KERN_INFO "Curso: Sistemas Operativos 1\n");
 }
  
